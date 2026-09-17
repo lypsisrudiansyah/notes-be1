3a61c23958a9e6ca4191c7cb9dcbc8901a26f024
rudiansyah <rudiansyahether@gmail.com>
2026-09-17 14:33:25 +0700

    feat: create 'arcade VPC Network Peering- GSP193.sh', 'arcade game Collect Metrics from Exporters using the Managed Service for Prometheus - GSP1026.sh' and 'arcade game Managing Deployments Using Kubernetes Engine - GSP053.sh'

---
+++ events/skillbadges/arcade VPC Network Peering- GSP193.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# VPC Network Peering- GSP193
+
+
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+clear
+
+
+# Ask user for required inputs
+read -p "${BOLD_TEXT}${YELLOW_TEXT}Enter ZONE: ${RESET_FORMAT}" ZONE
+read -p "${BOLD_TEXT}${YELLOW_TEXT}Enter ZONE_2: ${RESET_FORMAT}" ZONE_2
+read -p "${BOLD_TEXT}${YELLOW_TEXT}Enter PROJECT_ID_2: ${RESET_FORMAT}" PROJECT_ID_2
+
+export PROJECT_ID=$DEVSHELL_PROJECT_ID
+export REGION_1="${ZONE%-*}"
+export REGION_2="${ZONE_2%-*}"
+gcloud config set project $PROJECT_ID
+gcloud compute networks create network-a --subnet-mode custom
+gcloud compute networks subnets create network-a-subnet --network network-a \
+    --range 10.0.0.0/16 --region $REGION_1
+
+gcloud compute instances create vm-a --zone $ZONE --network network-a --subnet network-a-subnet --machine-type e2-small
+gcloud compute firewall-rules create network-a-fw --network network-a --allow tcp:22,icmp
+gcloud config set project $PROJECT_ID_2
+gcloud compute networks create network-b --subnet-mode custom
+gcloud compute networks subnets create network-b-subnet --network network-b \
+    --range 10.8.0.0/16 --region $REGION_2
+
+gcloud compute instances create vm-b --zone $ZONE_2 --network network-b --subnet network-b-subnet --machine-type e2-small
+gcloud compute firewall-rules create network-b-fw --network network-b --allow tcp:22,icmp
+gcloud config set project $PROJECT_ID
+
+gcloud compute networks peerings create peer-ab \
+    --network=network-a \
+    --peer-project=$PROJECT_ID_2 \
+    --peer-network=network-b 
+
+gcloud config set project $PROJECT_ID_2
+
+gcloud compute networks peerings create peer-ba \
+    --network=network-b \
+    --peer-project=$PROJECT_ID \
+    --peer-network=network-a
+
+echo "${CYAN_TEXT}${BOLD_TEXT}              LAB COMPLETED SUCCESSFULLY!              ${RESET_FORMAT}"
